2778ac49e97891d44c258b730a680b75c26bb301
NeerajQA <neeraj1264qa@gmail.com>
2025-01-09 11:58:57 +0530

    change products get request

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).json(products);
+    res.status(200).json({ message: 'Test endpoint working!' });
   } catch (error) {
     res.status(500).json({ message: 'Failed to fetch products', error });
   }
