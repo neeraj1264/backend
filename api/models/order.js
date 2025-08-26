05c8a18e25aff47d67397aa4401c7ce39bc648a5
neeraj1264 <121123848+neeraj1264@users.noreply.github.com>
2025-08-26 21:16:03 +0530

    Update order.js

--- api/models/order.js
+++ api/models/order.js
@@ -8,6 +8,12 @@ const orderSchema = new mongoose.Schema({
   phone: { type: Number },
   address: { type: String },
   timestamp: { type: String, required: true }, 
+  discount:{ type: Number, default: 0  },
+  delivery: { type: Number, default: 0  },
+  includeGST: { type: Boolean, default: true },
+  orderNumber: { type: String },
+  billNumber: { type: String },
+  orderType: {type: String},
 });
 
 const Order = mongoose.model('Order', orderSchema);
