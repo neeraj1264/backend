83a9ec0c2886e623452ae41d890e0c1e257a2c92
neeraj1264 <121123848+neeraj1264@users.noreply.github.com>
2025-11-12 14:56:50 +0530

    Update customerdataRoutes.js

--- api/routes/customerdataRoutes.js
+++ api/routes/customerdataRoutes.js
@@ -18,22 +18,22 @@ router.post('/', async (req, res) => {
       }
   
       // Create a new customer entry if no match is found
-      const newCustomer = new CustomerData({ id, timestamp, name, phone, address,   totalCash: Number(paidAmount || 0),
+      const customer = new CustomerData({ id, timestamp, name, phone, address,   totalCash: Number(paidAmount || 0),
         totalOwed: Number(creditAmount || 0),
         totalAmount: Number(paidAmount || 0) + Number(creditAmount || 0),
         transactions, });
 
-         if (name) newCustomer.name = name;
-    if (address) newCustomer.address = address;
+         if (name) customer.name = name;
+    if (address) customer.address = address;
 
     // Increment totals
-    newCustomer.totalCash = (newCustomer.totalCash || 0) + Number(paidAmount || 0);
-    newCustomer.totalOwed = (newCustomer.totalOwed || 0) + Number(creditAmount || 0);
-    newCustomer.totalAmount =
-      (newCustomer.totalCash || 0) + (newCustomer.totalOwed || 0);
+    customer.totalCash = (customer.totalCash || 0) + Number(paidAmount || 0);
+    customer.totalOwed = (customer.totalOwed || 0) + Number(creditAmount || 0);
+    customer.totalAmount =
+      (customer.totalCash || 0) + (customer.totalOwed || 0);
         
-      await newCustomer.save();
-      res.status(201).json({ message: 'Customer added successfully.', customer: newCustomer });
+      await customer.save();
+      res.status(201).json({ message: 'Customer added successfully.', customer });
     } catch (error) {
       console.error("Error saving customer:", error);
       res.status(500).json({ message: 'Failed to create customer', error });
